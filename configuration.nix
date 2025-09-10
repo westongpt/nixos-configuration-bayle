348f06999094301957b992a1a7356f1a0e3549b0
Weston Hanners <weston@hanners.us>
2025-09-09 20:03:54 -0400

    Added AppImage support.

--- configuration.nix
+++ configuration.nix
@@ -190,5 +190,15 @@
     rsync
   ];
 
+  # Support AppImages
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
+
   system.stateVersion = "25.05";
 }
