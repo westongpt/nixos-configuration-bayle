52f9e4792ce6549314c3d9e37ef3023a44d6708d
Weston Hanners <weston@hanners.us>
2026-01-15 17:25:51 -0500

    Vivaldi and 1Password

--- nix/programs.nix
+++ nix/programs.nix
@@ -20,6 +20,7 @@
     killall
     pkgs.openrgb
     clang
+    cargo
     pciutils
     pcmanfm
     helix
@@ -27,10 +28,28 @@
     vulkan-tools
     snixembed
     pkgs.dart
+    (pkgs.vivaldi.override { commandLineArgs = "--password-store=kwallet6"; })
     (pkgs.sddm-astronaut.override { embeddedTheme = "black_hole"; })
   ];
 
   security.pam.services.swaylock = { };
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "weston" ];
+  };
+
+  environment.etc = {
+    "1password/custom_allowed_browsers" = {
+      text = ''
+        vivaldi-bin
+      '';
+      mode = "0755";
+    };
+  };
 
   services.autorandr.enable = true;
   services.printing.enable = true;
